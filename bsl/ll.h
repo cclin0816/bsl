8dcab95ccac8040d41653d307bf7ea8a94b5b6b5
cclin0816 <benjamin.cclin@gmail.com>
2022-10-08 04:00:26 +0800

    tmp commit

--- bsl/ll.h
+++ bsl/ll.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <bsl/empty.h>
+#include <bsl/in_place.h>
 #include <config.h>
 
 #include <atomic>
+#include <utility>
 
 // thread safe
 
@@ -11,79 +13,144 @@ namespace bsl {
 
 // pre-declare
 
-template <typename _Tv>
+template <typename _Tval>
 class lln_t;
-template <typename _Tv>
+template <typename _Tval>
 class ll_t;
-template <typename _Tp>
+template <typename _Tnode>
 class ll_itr;
 
 // linked list node
-template <typename _Tv = empty_t>
+template <typename Tval = empty_t>
 class lln_t {
-  using type = lln_t<_Tv>;
-
-  template <typename _Tp>
+  template <typename _Tnode>
   friend class ll_itr;
-  template <typename _Tp>
+  template <typename _Tval>
   friend class ll_t;
 
- protected:
-  std::atomic<type *> next{PTR_FAIL};
+ public:
+  using value_type = Tval;
+  using type = lln_t<Tval>;
+
+ private:
+  std::atomic<type *> next{nullptr};
+  [[no_unique_address]] value_type val{};
 
  public:
-  [[no_unique_address]] _Tv val;
   lln_t() noexcept = default;
-  // this might fail to instantiate if copy or move constructor is used, since
-  // deleted
+
   template <typename... Args>
-  lln_t(Args &&...args) noexcept(noexcept(_Tv(std::forward<Args>(args)...)))
+  lln_t(in_place_t, Args &&...args) noexcept(
+      noexcept(value_type(std::forward<Args>(args)...)))
       : val(std::forward<Args>(args)...) {}
-  // no copy and move
+
   lln_t(const type &) = delete;
   lln_t(type &&) = delete;
   type &operator=(const type &) = delete;
   type &operator=(type &&) = delete;
 
-  void push(type *node) noexcept {
-    type *old = next.load(std::memory_order_relaxed);
+  [[nodiscard]] value_type &value() noexcept { return val; }
+  [[nodiscard]] const value_type &value() const noexcept { return val; }
+
+  [[nodiscard]] void *base() &noexcept {
+    return reinterpret_cast<void *>(this);
+  }
+};
+
+template <typename Tnode>
+class ll_itr {
+  using type = ll_itr<Tnode>;
+  using node_type = Tnode;
+  using value_type = node_type;
+
+ protected:
+  node_type *node = nullptr;
+
+ public:
+  ll_itr() noexcept = default;
+  ll_itr(node_type *node) noexcept : node(node) {}
+  type &operator=(node_type *node) noexcept {
+    this->node = node;
+    return *this;
+  }
+  type &operator++() noexcept {
+    node = node->next.load(std::memory_order_relaxed);
+  }
+  type operator++(int) &noexcept {
+    type tmp(*this);
+    ++(*this);
+    return tmp;
+  }
+  type operator+(const int n) noexcept {
+    type tmp(*this);
+    for (int i = 0; i < n; ++i) {
+      ++tmp;
+    }
+    return tmp;
+  }
+  type &operator+=(const int n) noexcept {
+    for (int i = 0; i < n; ++i) {
+      ++(*this);
+    }
+    return *this;
+  }
+  friend bool operator==(const type &lhs, const type &rhs) noexcept {
+    return lhs.node == rhs.node;
+  }
+  node_type &operator*() const noexcept { return *node; }
+  node_type *operator->() const noexcept { return node; }
+  operator node_type *() const noexcept { return node; }
+};
+
+template <typename Tval = empty_t>
+class ll_t {
+  using type = ll_t<Tval>;
+  using node_type = lln_t<Tval>;
+  using empty_type = lln_t<empty_t>;
+  using iterator = ll_itr<node_type>;
+  using const_iterator = ll_itr<const node_type>;
+  using value_type = Tval;
+
+ private:
+  empty_type head;
+
+ public:
+  ll_t() noexcept {
+    head.next.store((empty_type *)PTR_FAIL, std::memory_order_relaxed);
+  }
+  ll_t(const type &) = delete;
+  ll_t(type &&) = delete;
+  type &operator=(const type &) = delete;
+  type &operator=(type &&) = delete;
+
+  [[nodiscard]] bool empty() const noexcept {
+    return head.next.load(std::memory_order_relaxed) == PTR_FAIL;
+  }
+
+  void push(node_type *node) &noexcept {
+    auto *node_cast = reinterpret_cast<empty_type *>(node);
+    auto *old = head.next.load(std::memory_order_relaxed);
     while (true) {
-      node->next = old;
-      if (next.compare_exchange_weak(old, node, std::memory_order_release,
-                                     std::memory_order_relaxed)) {
+      node->next.store(old, std::memory_order_relaxed);
+      if (head.next.compare_exchange_weak(old, node_cast,
+                                          std::memory_order_release,
+                                          std::memory_order_relaxed)) {
         break;
       }
     }
   }
-};
+  void push(node_type &node) &noexcept { push(&node); }
+  void push(void *node) &noexcept { push(reinterpret_cast<node_type *>(node)); }
 
-template <typename _Tv>
-class ll_itr {
-  using type = ll_itr<_Tv>;
-
- protected:
-  _Tv *node = nullptr;
-
- public:
-  ll_itr() noexcept = default;
-  ll_itr(const type &) noexcept = default;
-  type &operator=(const type &) noexcept = default;
-  ll_itr(type &&) noexcept = default; 
-  type &operator=(type &&) noexcept = default;
-  ll_itr(_Tv *node) noexcept : node(node) {}
-  [[nodiscard]] bool operator==(const type &other) const noexcept {
-    return node == other.node;
+  node_type *pop() &noexcept {
+    auto *old = head.next.load(std::memory_order_relaxed);
+    while (true) {
+      if (old == (empty_type *)PTR_FAIL) {
+        return PTR_FAIL;
+      }
+      auto *next = old->next.load(std::memory_order_relaxed);
+    }
   }
-};
-
-template <typename _Tp = empty_t>
-class ll_t {
-  using type = ll_t<_Tp>;
-  using node_type = lln_t<_Tp>;
-  using empty_type = lln_t<empty_t>;
-
- private:
-  // empty_t head;
 };
 
 }  // namespace bsl
